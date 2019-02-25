38dcb9b626579878929eeeeec525e4582d7e090a
janekbechtel <janek.bechtel@student.kit.edu>
2019-02-25 11:02:21 +0100

    Fix patMuon trigger selection

--- Run2018ABC_CMSSW_10_2_0/merging.py
+++ Run2018ABC_CMSSW_10_2_0/merging.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.19 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
-# with command line options: PAT -s PAT --filein file:simulated_and_cleaned.root --fileout file:merged.root --era Run2_2018 --runUnscheduled --data --scenario pp --conditions 102X_dataRun2_Sep2018Rereco_v1 --eventcontent MINIAODSIM --datatier USER --customise TauAnalysis/MCEmbeddingTools/customisers.customiseMerging_Reselect --customise_commands process.patTrigger.processName = cms.string('SIMembedding') -n -1 --no_exec --python_filename=merging.py
+# with command line options: PAT -s PAT --filein file:simulated_and_cleaned.root --fileout file:merged.root --era Run2_2018 --runUnscheduled --data --scenario pp --conditions 102X_dataRun2_Sep2018Rereco_v1 --eventcontent MINIAODSIM --datatier USER --customise TauAnalysis/MCEmbeddingTools/customisers.customiseMerging_Reselect --customise_commands process.patTrigger.processName = cms.string('SIMembedding') \nprocess.slimmedPatTrigger.triggerResults =  cms.InputTag('TriggerResults::SIMembedding') \nprocess.patMuons.triggerResults =  cms.InputTag('TriggerResults::SIMembedding') -n -1 --no_exec --python_filename=merging.py
 import FWCore.ParameterSet.Config as cms
 
 from Configuration.StandardSequences.Eras import eras
@@ -178,7 +178,9 @@ process = miniAOD_customizeAllData(process)
 
 # Customisation from command line
 
-process.patTrigger.processName = cms.string('SIMembedding')
+process.patTrigger.processName = cms.string('SIMembedding') 
+process.slimmedPatTrigger.triggerResults =  cms.InputTag('TriggerResults::SIMembedding') 
+process.patMuons.triggerResults =  cms.InputTag('TriggerResults::SIMembedding')
 #Have logErrorHarvester wait for the same EDProducers to finish as those providing data for the OutputModule
 from FWCore.Modules.logErrorHarvester_cff import customiseLogErrorHarvesterUsingOutputCommands
 process = customiseLogErrorHarvesterUsingOutputCommands(process)
