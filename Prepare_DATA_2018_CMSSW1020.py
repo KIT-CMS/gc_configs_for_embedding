5eebd0e91813b4b6c1887d97d831ecfff5cab750
janekbechtel <janek.bechtel@student.kit.edu>
2019-02-26 16:46:52 +0100

    Revert "Soften generator level cuts"
    This reverts commit be6bc6babc482f875925b372455fc4ae1bf9f13d.

--- Prepare_DATA_2018_CMSSW1020.py
+++ Prepare_DATA_2018_CMSSW1020.py
@@ -4,16 +4,16 @@ from Prepare_all import finale_state
 
 
 if __name__ == "__main__":
-
-
-
-
-
-
-	final_states=["MuTau"]
+	final_states=["Preselection"]
+	for run in ["Run2018A","Run2018B","Run2018C"]:
+		dbs_map = {}
+		dbs_map["DoubleMuon_{}-v1".format(run)]="/DoubleMuon/{}-v1/RAW".format(run)
+		for finalstate in final_states:
+			finale_state(finalstate=finalstate, identifier="data_2018ABC_CMSSW1020", runs=[run], inputfolder="Run2018ABC_CMSSW_10_2_0", add_dbs=dbs_map, reselect=False)
+	final_states=["MuTau","ElTau","ElMu","TauTau","MuEmb","ElEmb"]
 	for finalstate in final_states:
 		finale_state(finalstate=finalstate, identifier="data_2018ABC_CMSSW1020", runs=["Run2018A","Run2018B","Run2018C"], inputfolder="Run2018ABC_CMSSW_10_2_0", add_dbs=None, reselect=False)
-	exit()
+
 	## Different globaltag for Run2018D
 
         final_states=["Preselection"]
