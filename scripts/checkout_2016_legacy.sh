006f53366ba227beca92d459bf18787e1a78d622
Sebastian Brommer <sebastian.brommer@kit.edu>
2019-08-12 11:43:19 +0200

    update checkout script

--- scripts/checkout_2016_legacy.sh
+++ scripts/checkout_2016_legacy.sh
@@ -4,9 +4,6 @@ source $VO_CMS_SW_DIR/cmsset_default.sh
 # needed for selection -> cleaning -> simulation -> merging into AODSIM
 scram project CMSSW_8_0_33
 
-# needed for AODSIM -> MINIAODSIM
-scram project CMSSW_9_4_14
-
 #first setup the 8_0_33 env
 cd CMSSW_8_0_33/src
 cmsenv
@@ -22,7 +19,12 @@ cd gc_configs_for_embedding
 python Prepare_DATA_2016_CMSSW8033.py
 
 # and now the 9_4_14 env
-cd ../../../CMSSW_9_4_14
+cd ../../../
+
+# needed for AODSIM -> MINIAODSIM
+scram project CMSSW_9_4_14
+cd CMSSW_9_4_14/src
+
 cmsenv
 git cms-merge-topic KIT-CMS:emb_2016_legacy_94x
 scramv1 b -j 12
