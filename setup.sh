97f39869aaafcaff5ffc88720a1f86158a4a61fd
Sebastian Brommer <sebastian.brommer@kit.edu>
2021-09-29 13:44:24 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+# source /cvmfs/sft-nightlies.cern.ch/lcg/views/dev3/latest/x86_64-centos7-gcc9-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_97python3/x86_64-centos7-gcc9-opt/setup.sh
+source /cvmfs/grid.cern.ch/umd-c7ui-latest/etc/profile.d/setup-c7-ui-example.sh
