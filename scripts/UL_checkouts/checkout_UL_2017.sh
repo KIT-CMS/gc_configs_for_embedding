7e35922c12989e499903d71ebf836c9c577b31e0
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-01-04 10:53:12 +0100

    add missing 2017 config

--- scripts/UL_checkouts/checkout_UL_2017.sh
+++ scripts/UL_checkouts/checkout_UL_2017.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
-source $VO_CMS_SW_DIR/cmsset_default.sh
-scram project CMSSW_10_6_28
-cd CMSSW_10_6_28/src
-eval `scramv1 runtime -sh`
+if [ ! -d "CMSSW_10_6_28" ]; then
+    export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+    source $VO_CMS_SW_DIR/cmsset_default.sh
+    scram project CMSSW_10_6_28
+    cd CMSSW_10_6_28/src
+    eval `scramv1 runtime -sh`
 
-git cms-init
-git cms-addpkg TauAnalysis/MCEmbeddingTools
-git cms-addpkg SimG4CMS
-git cms-addpkg SimG4Core
-git cms-merge-topic KIT-CMS:embedding_UL_10_6_28
+    git cms-init
+    git cms-addpkg TauAnalysis/MCEmbeddingTools
+    git cms-addpkg SimG4CMS
+    git cms-addpkg SimG4Core
+    git cms-merge-topic KIT-CMS:embedding_UL_10_6_28
 
-scramv1 b -j 12
+    scramv1 b -j 12
+fi
 
 # git clone git@github.com:KIT-CMS/grid-control.git
 # git clone git@github.com:KIT-CMS/gc_configs_for_embedding.git
--- scripts/UL_checkouts/checkout_UL_2017_HLT.sh
+++ scripts/UL_checkouts/checkout_UL_2017_HLT.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
-source $VO_CMS_SW_DIR/cmsset_default.sh
-scram project CMSSW_9_4_14_UL_patch1
-cd CMSSW_9_4_14_UL_patch1/src
-eval `scramv1 runtime -sh`
+if [ ! -d "CMSSW_9_4_14_UL_patch1" ]; then
+    export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+    source $VO_CMS_SW_DIR/cmsset_default.sh
+    scram project CMSSW_9_4_14_UL_patch1
+    cd CMSSW_9_4_14_UL_patch1/src
+    eval `scramv1 runtime -sh`
 
-git cms-init
-git cms-addpkg TauAnalysis/MCEmbeddingTools
-git cms-addpkg SimG4CMS
-git cms-addpkg SimG4Core
-git cms-merge-topic KIT-CMS:embedding_9_4_14_UL_patch1
+    git cms-init
+    git cms-addpkg TauAnalysis/MCEmbeddingTools
+    git cms-addpkg SimG4CMS
+    git cms-addpkg SimG4Core
+    #git cms-merge-topic KIT-CMS:embedding_9_4_14_UL_patch1
 
-scramv1 b -j 12
-
+    scramv1 b -j 12
+fi
