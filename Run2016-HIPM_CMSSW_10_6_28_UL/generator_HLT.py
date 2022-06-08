02114b4bcc16a27091b3397526143c429e2df9cb
Sebastian Brommer <sebastian.brommer@kit.edu>
2022-06-08 11:19:07 +0200

    remove remnant

--- Run2016-HIPM_CMSSW_10_6_28_UL/generator_HLT.py
+++ Run2016-HIPM_CMSSW_10_6_28_UL/generator_HLT.py
@@ -71,7 +71,6 @@ process.source = cms.Source(
         "drop *_ctppsDiamondRawToDigi_*_*",
         "drop *_siPixelDigis_*_*",
         "drop *_tcdsDigis_*_*",
-        "drop *_ak4PFJetsCHS_rho_LHEembeddingCLEAN",
     ),
 )
 
