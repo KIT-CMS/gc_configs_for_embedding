d7028e9dfaf16da937c24a016cd45890c0652f1c
Moritz Molch <moritz.molch@cern.ch>
2024-02-06 17:21:59 +0100

    Update filter bits in MINIAOD -> NANOAOD config

--- Run2018_CMSSW_10_6_28_UL/embedding_nanoaod.py
+++ Run2018_CMSSW_10_6_28_UL/embedding_nanoaod.py
@@ -97,120 +97,156 @@ process.rawPuppiMetTable.src = cms.InputTag("slimmedMETsPuppi", "", "RERUNPUPPI"
 process.slimmedMETsPuppi.t01Variation = cms.InputTag("slimmedMETsPuppi", "", "RERUNPUPPI")
 process.metrawCaloPuppi.metSource = cms.InputTag("slimmedMETsPuppi", "", "RERUNPUPPI")
 process.pfMetPuppi.metSource = cms.InputTag("slimmedMETsPuppi", "", "RERUNPUPPI")
-# add modifications from https://github.com/cms-sw/cmssw/pull/38472 to correctly handle trigger flags in NanoAOD
-process.triggerObjectTable.selections[0].qualityBits = cms.string(
-                            "filter('*CaloIdLTrackIdLIsoVL*TrackIso*Filter') + " \
-                            "2*filter('hltEle*WPTight*TrackIsoFilter*') + " \
-                            "4*filter('hltEle*WPLoose*TrackIsoFilter') + " \
-                            "8*filter('*OverlapFilter*IsoEle*PFTau*') + " \
-                            "16*filter('hltEle*Ele*CaloIdLTrackIdLIsoVL*Filter') + " \
-                            "32*filter('hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*')  + " \
-                            "64*filter('hlt*OverlapFilterIsoEle*PFTau*') + " \
-                            "128*filter('hltEle*Ele*Ele*CaloIdLTrackIdLDphiLeg*Filter') + " \
-                            "256*max(filter('hltL3fL1Mu*DoubleEG*Filtered*'),filter('hltMu*DiEle*CaloIdLTrackIdLElectronleg*Filter')) + " \
-                            "512*max(filter('hltL3fL1DoubleMu*EG*Filter*'),filter('hltDiMu*Ele*CaloIdLTrackIdLElectronleg*Filter')) + " \
-                            "1024*min(filter('hltEle32L1DoubleEGWPTightGsfTrackIsoFilter'),filter('hltEGL1SingleEGOrFilter')) + " \
-                            "2048*filter('hltEle*CaloIdVTGsfTrkIdTGsfDphiFilter') + " \
-                            "4096*path('HLT_Ele*PFJet*') + " \
-                            "8192*max(filter('hltEG175HEFilter'),filter('hltEG200HEFilter'))")
 
+# Customisation from command line
+
+# add additional filters to TrigObj_filterBits column in NANOAOD
+# these filters are needed in order to process the correct trigger object matching for applying
+# the trigger scale factors on embedding samples
+
+# modify the electron entry
+process.triggerObjectTable.selections[0].qualityBits = cms.string(
+    "filter('*CaloIdLTrackIdLIsoVL*TrackIso*Filter') + " \
+    "2*filter('hltEle*WPTight*TrackIsoFilter*') + " \
+    "4*filter('hltEle*WPLoose*TrackIsoFilter') + " \
+    "8*filter('*OverlapFilter*IsoEle*PFTau*') + " \
+    "16*filter('hltEle*Ele*CaloIdLTrackIdLIsoVL*Filter') + " \
+    "32*filter('hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*')  + " \
+    "64*filter('hltOverlapFilterIsoEle*PFTau*') + " \
+    "128*filter('hltEle*Ele*Ele*CaloIdLTrackIdLDphiLeg*Filter') + " \
+    "256*max(filter('hltL3fL1Mu*DoubleEG*Filtered*'),filter('hltMu*DiEle*CaloIdLTrackIdLElectronleg*Filter')) + " \
+    "512*max(filter('hltL3fL1DoubleMu*EG*Filter*'),filter('hltDiMu*Ele*CaloIdLTrackIdLElectronleg*Filter')) + " \
+    "1024*min(filter('hltEle32L1DoubleEGWPTightGsfTrackIsoFilter'),filter('hltEGL1SingleEGOrFilter')) + " \
+    "2048*filter('hltEle*CaloIdVTGsfTrkIdTGsfDphiFilter') + " \
+    "4096*path('HLT_Ele*PFJet*') + " \
+    "8192*max(filter('hltEG175HEFilter'),filter('hltEG200HEFilter')) + " \
+    "16384*filter('hltEle27WPTightGsfTrackIsoFilter') + " \
+    "32768*filter('hltEle32WPTightGsfTrackIsoFilter') + " \
+    "65536*filter('hltEle35noerWPTightGsfTrackIsoFilter') + " \
+    "131072*filter('hltEle32L1DoubleEGWPTightGsfTrackIsoFilter') + " \
+    "262144*filter('hltEle24erWPTightGsfTrackIsoFilterForTau')" \
+)
+
+# add documentation for electron filter bits
+process.triggerObjectTable.selections[0].qualityBitsDoc = cms.string(
+    "1 = CaloIdL_TrackIdL_IsoVL, " \
+    "2 = 1e (WPTight), " \
+    "4 = 1e (WPLoose), " \
+    "8 = OverlapFilter PFTau, " \
+    "16 = 2e, " \
+    "32 = 1e-1mu, " \
+    "64 = 1e-1tau, " \
+    "128 = 3e, " \
+    "256 = 2e-1mu, " \
+    "512 = 1e-2mu, " \
+    "1024 = 1e (32_L1DoubleEG_AND_L1SingleEGOr), " \
+    "2048 = 1e (CaloIdVT_GsfTrkIdT), " \
+    "4096 = 1e (PFJet), " \
+    "8192 = 1e (Photon175_OR_Photon200), " \
+    "16384 = 1e (for e leg trigger object matching in embedding, path HLT_Ele27_WPTight_Gsf), " \
+    "32768 = 1e (for e leg trigger object matching in embedding, path HLT_Ele32_WPTight_Gsf), " \
+    "65536 = 1e (for e leg trigger object matching in embedding, path HLT_Ele35_WPTight_Gsf), " \
+    "131072 = 1e (for e leg trigger object matching in embedding, path HLT_Ele32_WPTight_L1DoubleEG), " \
+    "262144 = e-tau (for e leg trigger object matching in embedding, path HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau*30_eta2p1_CrossL1)"
+)
+
+# modify the muon entry
 process.triggerObjectTable.selections[2].qualityBits = cms.string(
-                            "filter('*RelTrkIsoVVLFiltered0p4') + " \
-                            "2*filter('hltL3crIso*Filtered0p07') + " \
-                            "4*filter('*OverlapFilterIsoMu*PFTau*') + " \
-                            "8*max(filter('hltL3crIsoL1*SingleMu*Filtered0p07'),filter('hltL3crIsoL1sMu*Filtered0p07')) + " \
-                            "16*filter('hltDiMuon*Filtered*') + " \
-                            "32*filter('hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*') + " \
-                            "64*filter('hlt*OverlapFilterIsoMu*PFTau*') + " \
-                            "128*filter('hltL3fL1TripleMu*') + " \
-                            "256*max(filter('hltL3fL1DoubleMu*EG*Filtered*'),filter('hltDiMu*Ele*CaloIdLTrackIdLElectronleg*Filter')) + " \
-                            "512*max(filter('hltL3fL1Mu*DoubleEG*Filtered*'),filter('hltMu*DiEle*CaloIdLTrackIdLElectronleg*Filter')) + " \
-                            "1024*max(filter('hltL3fL1sMu*L3Filtered50*'),filter('hltL3fL1sMu*TkFiltered50*')) + " \
-                            "2048*max(filter('hltL3fL1sMu*L3Filtered100*'),filter('hltL3fL1sMu*TkFiltered100*'))")
-process.triggerObjectTable.selections[3] = cms.PSet(
-            name = cms.string("Tau"),
-            id = cms.int32(15),
-            sel = cms.string("type(84) && pt > 5 && coll('*Tau*') && ( filter('*LooseChargedIso*') || filter('*MediumChargedIso*') || filter('*DeepTau*') || filter('*TightChargedIso*') || filter('*TightOOSCPhotons*') || filter('hltL2TauIsoFilter') || filter('*OverlapFilterIsoMu*') || filter('*OverlapFilterIsoEle*') || filter('*L1HLTMatched*') || filter('*Dz02*') || filter('*DoublePFTau*') || filter('*SinglePFTau*') || filter('hlt*SelectedPFTau') || filter('*DisplPFTau*') || filter('*Tau*') )"), #All trigger objects from a Tau collection + passing at least one filter
-            # sel = cms.string("type(84) && pt > 5 && coll('*Tau*') && filter('*Tau*') "), #All trigger objects from a Tau collection + passing at least one filter
-            l1seed = cms.string("type(-100)"),
-            l1deltaR = cms.double(0.3),
-            l2seed = cms.string("type(84) && coll('hltL2TauJetsL1IsoTauSeeded')"),
-            l2deltaR = cms.double(0.3),
-            skipObjectsNotPassingQualityBits = cms.bool(True),
-            qualityBits = cms.string(
-                            "filter('*LooseChargedIso*') + " \
-                            "2*filter('*MediumChargedIso*') + " \
-                            "4*filter('*TightChargedIso*') + " \
-                            "8*filter('*DeepTau*') + " \
-                            "16*filter('*TightOOSCPhotons*') + " \
-                            "32*filter('*Hps*') + " \
-                            "64*filter('hlt*DoublePFTau*TrackPt1*ChargedIsolation*Dz02*') + " \
-                            "128*filter('hlt*DoublePFTau*DeepTau*L1HLTMatched') + " \
-                            "256*filter('hlt*OverlapFilterIsoEle*WPTightGsf*PFTau') + " \
-                            "512*filter('hlt*OverlapFilterIsoMu*PFTau*') + " \
-                            "1024*filter('hlt*SelectedPFTau*L1HLTMatched') + " \
-                            "2048*filter('hlt*DoublePFTau*TrackPt1*ChargedIso*') + " \
-                            "4096*filter('hlt*DoublePFTau*Track*ChargedIso*AgainstMuon') + " \
-                            "8192*filter('hltHpsSinglePFTau*HLTMatched') + " \
-                            "16384*filter('hltHpsOverlapFilterDeepTauDoublePFTau*PFJet*') + " \
-                            "32768*filter('hlt*Double*ChargedIsoDisplPFTau*Dxy*') + " \
-                            "65536*filter('*Monitoring') + " \
-                            "131072*filter('*Reg') + " \
-                            "262144*filter('*L1Seeded') + " \
-                            "524288*filter('*1Prong') + " \
-                            "1048576*filter('*DoubleL2IsoTau26*') + " \
-                            "2097152*filter('*SingleL2IsoTau20*') + " \
-                            "4194304*filter('*TauJet*') "),
-            # qualityBits = cms.string("5000*filter('*DoubleL2IsoTau26*')"),
-            qualityBitsDoc = cms.string("1 = LooseChargedIso, 2 = MediumChargedIso, 4 = TightChargedIso, 8 = DeepTau, 16 = TightID OOSC photons, 32 = HPS, 64 = charged iso di-tau, 128 = deeptau di-tau, 256 = e-tau, 512 = mu-tau, 1024 = single-tau/tau+MET, 2048 = run 2 VBF+ditau, 4096 = run 3 VBF+ditau, 8192 = run 3 double PF jets + ditau, 16384 = di-tau + PFJet, 32768 = Displaced Tau, 65536 = Monitoring, 131072 = regional paths, 262144 = L1 seeded paths, 524288 = 1 prong tau paths, 1048576 = double L2 tau26 (for tau embedding), 2097152 = single L2 tau20 (for tauleg in mu + tau crosstrigger in embedding), 4194304 = tau jet5 (for tauleg in e + tau crosstrigger in embedding)"),
-        )
-process.triggerObjectTable.selections[4].qualityBits = cms.string(
-                "1         * filter('*CrossCleaned*LooseChargedIsoPFTau*') + " \
-                "2         * filter('hltBTagCaloCSVp087Triple') + " \
-                "4         * filter('hltDoubleCentralJet90') + " \
-                "8         * filter('hltDoublePFCentralJetLooseID90') + " \
-                "16        * filter('hltL1sTripleJetVBFIorHTTIorDoubleJetCIorSingleJet') + " \
-                "32        * filter('hltQuadCentralJet30') + " \
-                "64        * filter('hltQuadPFCentralJetLooseID30') + " \
-                "128       * max(filter('hltL1sQuadJetC50IorQuadJetC60IorHTT280IorHTT300IorHTT320IorTripleJet846848VBFIorTripleJet887256VBFIorTripleJet927664VBF'), filter('hltL1sQuadJetCIorTripleJetVBFIorHTT')) + " \
-                "256       * filter('hltQuadCentralJet45') + " \
-                "512       * filter('hltQuadPFCentralJetLooseID45') + " \
-                "1024      * max(filter('hltL1sQuadJetC60IorHTT380IorHTT280QuadJetIorHTT300QuadJet'), filter('hltL1sQuadJetC50to60IorHTT280to500IorHTT250to340QuadJet')) + " \
-                "2048      * max(filter('hltBTagCaloCSVp05Double'), filter('hltBTagCaloDeepCSVp17Double')) + " \
-                "4096      * filter('hltPFCentralJetLooseIDQuad30') + " \
-                "8192      * filter('hlt1PFCentralJetLooseID75') + " \
-                "16384     * filter('hlt2PFCentralJetLooseID60') + " \
-                "32768     * filter('hlt3PFCentralJetLooseID45') + " \
-                "65536     * filter('hlt4PFCentralJetLooseID40') + " \
-                "131072    * max(filter('hltBTagPFCSVp070Triple'), max(filter('hltBTagPFDeepCSVp24Triple'), filter('hltBTagPFDeepCSV4p5Triple')) )+ " \
-                "262144    * filter('hltHpsOverlapFilterDeepTauDoublePFTau*PFJet*') + " \
-                "524288   * filter('*CrossCleaned*MediumDeepTauDitauWPPFTau*') + " \
-                "1048576   * filter('*CrossCleanedUsingDiJetCorrChecker*')"
-                )
-process.triggerObjectTable.selections[4].qualityBitsDoc = cms.string(
-                "Jet bits: bit 0 for VBF cross-cleaned from loose iso PFTau, bit 1 for hltBTagCaloCSVp087Triple, bit 2 for hltDoubleCentralJet90, bit 3 for hltDoublePFCentralJetLooseID90," \
-                " bit 4 for hltL1sTripleJetVBFIorHTTIorDoubleJetCIorSingleJet, bit 5 for hltQuadCentralJet30, bit 6 for hltQuadPFCentralJetLooseID30," \
-                " bit 7 for hltL1sQuadJetC50IorQuadJetC60IorHTT280IorHTT300IorHTT320IorTripleJet846848VBFIorTripleJet887256VBFIorTripleJet927664VBF or hltL1sQuadJetCIorTripleJetVBFIorHTT," \
-                " bit 8 for hltQuadCentralJet45, bit 9 for hltQuadPFCentralJetLooseID45," \
-                " bit 10  for hltL1sQuadJetC60IorHTT380IorHTT280QuadJetIorHTT300QuadJet or hltL1sQuadJetC50to60IorHTT280to500IorHTT250to340QuadJet" \
-                " bit 11 for hltBTagCaloCSVp05Double or hltBTagCaloDeepCSVp17Double, bit 12 for hltPFCentralJetLooseIDQuad30, bit 13 for hlt1PFCentralJetLooseID75," \
-                " bit 14 for hlt2PFCentralJetLooseID60, bit 15 for hlt3PFCentralJetLooseID45, bit 16 for hlt4PFCentralJetLooseID40," \
-                " bit 17 for hltBTagPFCSVp070Triple or hltBTagPFDeepCSVp24Triple or hltBTagPFDeepCSV4p5Triple,"\
-                " bit 18 for Double tau + jet,"\
-                " bit 19 for VBF cross-cleaned from medium deeptau PFTau,"+
-                " bit 20 for VBF cross-cleaned using dijet correlation checker ")
-process.triggerObjectTable.selections.append(
-            cms.PSet(
-            name = cms.string("BoostedTau"),
-            id = cms.int32(1515),
-            sel = cms.string("type(85) && pt > 120 && coll('hltAK8PFJetsCorrected') && filter('hltAK8SinglePFJets*SoftDropMass40*ParticleNetTauTau')"),
-            l1seed = cms.string("type(-99)"), l1deltaR = cms.double(0.3),
-            l2seed = cms.string("type(85)  && coll('hltAK8CaloJetsCorrectedIDPassed')"),  l2deltaR = cms.double(0.3),
-            skipObjectsNotPassingQualityBits = cms.bool(True),
-            qualityBits = cms.string(
-                "filter('hltAK8SinglePFJets*SoftDropMass40*ParticleNetTauTau')"
-                ),
-            qualityBitsDoc = cms.string("Bit 0 for HLT_AK8PFJetX_SoftDropMass40_PFAK8ParticleNetTauTau0p30")))
+    "filter('*RelTrkIsoVVLFiltered0p4') + " \
+    "2*filter('hltL3crIso*Filtered0p07') + " \
+    "4*filter('*OverlapFilterIsoMu*PFTau*') + " \
+    "8*max(filter('hltL3crIsoL1*SingleMu*Filtered0p07'),filter('hltL3crIsoL1sMu*Filtered0p07')) + " \
+    "16*filter('hltDiMuon*Filtered*') + " \
+    "32*filter('hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*') + " \
+    "64*filter('hltOverlapFilterIsoMu*PFTau*') + " \
+    "128*filter('hltL3fL1TripleMu*') + " \
+    "256*max(filter('hltL3fL1DoubleMu*EG*Filtered*'),filter('hltDiMu*Ele*CaloIdLTrackIdLElectronleg*Filter')) + " \
+    "512*max(filter('hltL3fL1Mu*DoubleEG*Filtered*'),filter('hltMu*DiEle*CaloIdLTrackIdLElectronleg*Filter')) + " \
+    "1024*max(filter('hltL3fL1sMu*L3Filtered50*'),filter('hltL3fL1sMu*TkFiltered50*')) + " \
+    "2048*max(filter('hltL3fL1sMu*L3Filtered100*'),filter('hltL3fL1sMu*TkFiltered100*')) + " \
+    "4096*filter('hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07') + " \
+    "8192*filter('hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07') + " \
+    "16384*filter('hltL3crIsoL1sMu18erTau24erIorMu20erTau24erL1f0L2f10QL3f20QL3trkIsoFiltered0p07') + " \
+    "32768*filter('hltL3crIsoBigORMu18erTauXXer2p1L1f0L2f10QL3f20QL3trkIsoFiltered0p07')"
+)
+
+# add documentation for muon filter bits
+process.triggerObjectTable.selections[2].qualityBitsDoc = cms.string(
+    "1 = TrkIsoVVL, " \
+    "2 = Iso, " \
+    "4 = OverlapFilter PFTau, " \
+    "8 = 1mu, " \
+    "16 = 2mu, " \
+    "32 = 1mu-1e, " \
+    "64 = 1mu-1tau, " \
+    "128 = 3mu, " \
+    "256 = 2mu-1e, " \
+    "512 = 1mu-2e, " \
+    "1024 = 1mu (Mu50), " \
+    "2048 = 1mu (Mu100), " \
+    "4096 = 1mu (for mu leg trigger object matching in embedding, path HLT_IsoMu24), " \
+    "8192 = 1mu (for mu leg trigger object matching in embedding, path HLT_IsoMu27), " \
+    "16384 = mu-tau (for mu leg trigger object matching in embedding, path HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 2017), " \
+    "32768 = mu-tau (for mu leg trigger object matching in embedding, path HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1 2018)"
+)
+
+# modify the selection string in the tau entry
+process.triggerObjectTable.selections[3].sel = cms.string(
+    "( type(84) || type(-100) ) && " \
+    "(pt > 5) && " \
+    "coll('*Tau*') &&" \
+    "( filter('*LooseChargedIso*') || " \
+    "filter('*MediumChargedIso*') || " \
+    "filter('*TightChargedIso*') || " \
+    "filter('*TightOOSCPhotons*') || " \
+    "filter('hltL2TauIsoFilter') || " \
+    "filter('*OverlapFilterIsoMu*') || " \
+    "filter('*OverlapFilterIsoEle*') || " \
+    "filter('*L1HLTMatched*') || " \
+    "filter('*Dz02*') || " \
+    "filter('*DoublePFTau*') || " \
+    "filter('*SinglePFTau*') || " \
+    "filter('hlt*SelectedPFTau') || " \
+    "filter('*DisplPFTau*') || " \
+    "filter('*Tau*') )"
+)
+
+# modify the tau entry
+process.triggerObjectTable.selections[3].qualityBits = cms.string(
+    "filter('*LooseChargedIso*') + " \
+    "2*filter('*MediumChargedIso*') + " \
+    "4*filter('*TightChargedIso*') + " \
+    "8*filter('*TightOOSCPhotons*') + " \
+    "16*filter('*Hps*') + " \
+    "32*filter('hltSelectedPFTau*MediumChargedIsolationL1HLTMatched*') + " \
+    "64*filter('hltDoublePFTau*TrackPt1*ChargedIsolation*Dz02Reg') + " \
+    "128*filter('hltOverlapFilterIsoEle*PFTau*') + " \
+    "256*filter('hltOverlapFilterIsoMu*PFTau*') + " \
+    "512*filter('hltDoublePFTau*TrackPt1*ChargedIsolation*') + " \
+    "1024*filter('hltL1sBigORLooseIsoEGXXerIsoTauYYerdRMin0p3') + " \
+    "2048*filter('hltL1sMu18erTau24erIorMu20erTau24er') + "
+    "4096*filter('hltL1sBigORMu18erTauXXer2p1') + " \
+    "8192*filter('hltDoubleL2IsoTau26eta2p2')"
+)
+
+# add documentation for tau filter bits
+process.triggerObjectTable.selections[3].qualityBitsDoc = cms.string(
+    "1 = LooseChargedIso, " \
+    "2 = MediumChargedIso, " \
+    "4 = TightChargedIso, " \
+    "8 = TightID OOSC photons, " \
+    "16 = HPS, " \
+    "32 = single-tau + tau+MET, " \
+    "64 = di-tau, " \
+    "128 = e-tau, " \
+    "256 = mu-tau, " \
+    "512 = VBF+di-tau, " \
+    "1024 = e-tau (for tau leg trigger object matching in embedding, path HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTau30_eta2p1), " \
+    "2048 = mu-tau (for tau leg trigger object matching in embedding, path HLT_IsoMu20_eta2p1_LooseChargedIsoPFTau27_eta2p1_CrossL1 2017), " \
+    "4096 = mu-tau (for tau leg trigger object matching in embedding, path HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1 2018), " \
+    "8192 = di-tau (for tau leg trigger object matching in embedding, paths HLT_Double*ChargedIsoPFTau*_Trk1*_eta2p1_Reg)"
+)
+
 # End of customisation functions
 
 # Customisation from command line
